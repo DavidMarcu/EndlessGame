3e39cd9a5a45e18acc8ae750f9399004690e984e
codrinalex1994 <codrinalex1994@yahoo.com>
2018-07-04 23:01:57 +0300

    Create portals and more

--- EndlessGame/Assets/Scripts/CreatePortal.cs
+++ EndlessGame/Assets/Scripts/CreatePortal.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class CreatePortal : MonoBehaviour {
 
+    public GameObject portal;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "FakeObstacle")
+        Transform otherTransform = other.gameObject.transform;
+        if (other.gameObject.tag == "FakeObstacle")
         {
-            Destroy(other.gameObject);
-            Destroy(gameObject);
+            GameObject newPortal = Instantiate(portal, new Vector3(this.transform.position.x, otherTransform.position.y, otherTransform.position.z), Quaternion.identity);
+            MovingUp movingUpScr = newPortal.GetComponent<MovingUp>();
+            MovingUp otherMovingUpScr = other.gameObject.GetComponent<MovingUp>();
+            movingUpScr.speed = otherMovingUpScr.speed;
         }
+            Destroy(gameObject);
     }
 }

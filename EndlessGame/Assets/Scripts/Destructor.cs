fbd1634bb0b28aaf5eee92063f0d97689a849915
codrinalex1994 <codrinalex1994@yahoo.com>
2018-07-05 20:59:58 +0300

    Player can go through portals

---
+++ EndlessGame/Assets/Scripts/Destructor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructor : MonoBehaviour {
+
+    private bool touchingPortal = false;
+    public float wrapSpace = 0.3f;
+    private float lastPlayerSpeed;
+    private Movement playerMovement;
+
+    private void Start()
+    {
+        playerMovement = this.GetComponent<Movement>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(!touchingPortal)
+        {
+            if(collision.gameObject.tag == "FakeObstacle" ||
+               collision.gameObject.tag == "TrueObstacle")
+            {
+                Time.timeScale = 0;
+            }
+        }
+
+        if (collision.gameObject.tag == "Portal")
+        {
+            if(Mathf.Abs(collision.gameObject.transform.position.x - this.transform.position.x) < wrapSpace)
+            {
+                //Debug.Log("collision");
+                touchingPortal = true;
+                lastPlayerSpeed = playerMovement.speed;
+                playerMovement.speed = 0;
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Portal")
+        {
+            touchingPortal = false;
+            playerMovement.speed = lastPlayerSpeed;
+        }
+    }
+
+}

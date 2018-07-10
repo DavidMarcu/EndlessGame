434666f44be87350d0924c810080c1f710b5adc5
codrinalex1994 <codrinalex1994@yahoo.com>
2018-07-10 22:59:50 +0300

    Increase speed with time pass

--- EndlessGame/Assets/Scripts/GlobalVars.cs
+++ EndlessGame/Assets/Scripts/GlobalVars.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GlobalVars {
+public class GlobalVars{
 
     public static float speed = 4f;
 
---
+++ EndlessGame/Assets/Scripts/IncreaseSpeed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreaseSpeed : MonoBehaviour {
+
+    private float speedInc = 0.1f;
+
+	void Update () {
+
+        if(GlobalVars.speed < 10)
+        {
+            GlobalVars.speed += speedInc * Time.deltaTime;
+        }
+	}
+}

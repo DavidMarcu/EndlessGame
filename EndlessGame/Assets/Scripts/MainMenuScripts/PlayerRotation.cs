919ecc42a7dfabbb6c4b3bc835b560aeb9de5d46
codrinalex1994 <codrinalex1994@yahoo.com>
2018-07-21 17:59:51 +0300

    Transition between menu and game, Directional Lights, and more

---
+++ EndlessGame/Assets/Scripts/MainMenuScripts/PlayerRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour {
+
+    public float rotateSpeed = 30f;
+
+	void Update () {
+        transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.World);
+    }
+}
